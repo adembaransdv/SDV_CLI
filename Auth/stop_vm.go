92666a0244530bbab9c8be50936ae29c4bd3705e
yasmine abbas <abbas@Yasmine>
2025-05-22 15:02:01 +0200

    modif stop

--- Auth/stop_vm.go
+++ Auth/stop_vm.go
@@ -1,104 +1,116 @@
 package vmware
 
 import (
-	"encoding/base64"
-	"encoding/json"
-	"errors"
-	"fmt"
-	"io"
-	"net/http"
-	"gopkg.in/ini.v1"
+    "encoding/base64"
+    "encoding/json"
+    "errors"
+    "fmt"
+    "io"
+    "net/http"
+    "crypto/tls"
+    "gopkg.in/ini.v1"
 )
 
-const host string = "http://127.0.0.1:5000"
+const host string = "https://192.168.1.3"
 
 type client struct {
-	Token string
+    Token string
 }
 
 type sessionResponse struct {
-	Value string `json:"value"`
+    Value string `json:"value"`
 }
 
 func StopVM(c *client, vmID string) error {
-	url := fmt.Sprintf("%s/rest/vcenter/vm/%s/power/stop", host, vmID)
-	req, err := http.NewRequest("POST", url, nil)
-	if err != nil {
-		return errors.New("erreur lors de la création de la requête POST")
-	}
-	req.Header.Add("Authorization", "Bearer "+c.Token)
+    url := fmt.Sprintf("%s/rest/vcenter/vm/%s/power/stop", host, vmID)
+    req, err := http.NewRequest("POST", url, nil)
+    if err != nil {
+        return errors.New("erreur lors de la création de la requête POST")
+    }
+    // je teste le tocken  qui se creer 
+    req.Header.Add("vmware-api-session-id", c.Token)
 
-	clientHTTP := &http.Client{}
-	resp, err := clientHTTP.Do(req)
-	if err != nil {
-		return fmt.Errorf("erreur lors de l'appel HTTP : %v", err)
-	}
-	defer resp.Body.Close()
+    clientHTTP := &http.Client{
+        Transport: &http.Transport{
+            TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+        },
+    }
 
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return fmt.Errorf("erreur lors de la lecture de la réponse : %v", err)
-	}
+    resp, err := clientHTTP.Do(req)
+    if err != nil {
+        return fmt.Errorf("erreur lors de l'appel HTTP : %v", err)
+    }
+    defer resp.Body.Close()
 
-	if resp.StatusCode != 200 {
-		return fmt.Errorf("erreur lors de l'arrêt de la VM, code : %d, message : %s", resp.StatusCode, string(body))
-	}
+    body, err := io.ReadAll(resp.Body)
+    if err != nil {
+        return fmt.Errorf("erreur lors de la lecture de la réponse : %v", err)
+    }
 
-	// Optionnel : afficher la réponse reçue
-	fmt.Println("Réponse de l'API StopVM :", string(body))
+    if resp.StatusCode != 200 {
+        return fmt.Errorf("erreur lors de l'arrêt de la VM, code : %d, message : %s", resp.StatusCode, string(body))
+    }
 
-	return nil
+    fmt.Println("Réponse de l'API StopVM :", string(body))
+    return nil
 }
 
 func CheckConfiguration() (*client, error) {
-	cfg, err := ini.Load("config.ini")
-	if err != nil {
-		return nil, fmt.Errorf("erreur lors de la lecture du fichier de configuration INI : %v", err)
-	}
+    cfg, err := ini.Load("config.ini")
+    if err != nil {
+        return nil, fmt.Errorf("erreur lors de la lecture du fichier de configuration INI : %v", err)
+    }
 
-	login := cfg.Section("vmware").Key("login").String()
-	password := cfg.Section("vmware").Key("password").String()
+    login := cfg.Section("vmware").Key("login").String()
+    password := cfg.Section("vmware").Key("password").String()
 
-	if login == "" || password == "" {
-		return nil, errors.New("les informations de connexion sont manquantes dans le fichier config.ini")
-	}
+    if login == "" || password == "" {
+        return nil, errors.New("les informations de connexion sont manquantes dans le fichier config.ini")
+    }
 
-	return Connection(login, password)
+    return Connection(login, password)
 }
 
 func Connection(login string, pass string) (*client, error) {
-	url := host + "/rest/com/vmware/cis/session"
+    url := host + "/rest/com/vmware/cis/session"
 
-	req, err := http.NewRequest("POST", url, nil)
-	if err != nil {
-		return nil, errors.New("erreur lors de la création de la requête")
-	}
+    req, err := http.NewRequest("POST", url, nil)
+    if err != nil {
+        return nil, errors.New("erreur lors de la création de la requête")
+    }
 
-	auth := login + ":" + pass
-	authEncoded := base64.StdEncoding.EncodeToString([]byte(auth))
-	req.Header.Add("Authorization", "Basic "+authEncoded)
+    auth := login + ":" + pass
+    authEncoded := base64.StdEncoding.EncodeToString([]byte(auth))
+    req.Header.Add("Authorization", "Basic "+authEncoded)
 
-	clienthttp := &http.Client{}
-	resp, err := clienthttp.Do(req)
-	if err != nil {
-		fmt.Println("Erreur lors de la requête POST :", err)
-		return nil, errors.New("erreur lors de l'appel HTTP")
-	}
-	defer resp.Body.Close()
+    clienthttp := &http.Client{
+        Transport: &http.Transport{
+            TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+        },
+    }
 
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return nil, errors.New("erreur lors de la lecture de la réponse")
-	}
+    resp, err := clienthttp.Do(req)
+    if err != nil {
+        fmt.Println("Erreur lors de la requête POST :", err)
+        return nil, errors.New("erreur lors de l'appel HTTP")
+    }
+    defer resp.Body.Close()
 
-	var session sessionResponse
-	err = json.Unmarshal(body, &session)
-	if err != nil {
-		return nil, errors.New("erreur lors du parsing JSON")
-	}
+    body, err := io.ReadAll(resp.Body)
+    if err != nil {
+        return nil, errors.New("erreur lors de la lecture de la réponse")
+    }
 
-	c := &client{
-		Token: session.Value,
-	}
-	return c, nil
+    var session sessionResponse
+    err = json.Unmarshal(body, &session)
+    if err != nil {
+        return nil, errors.New("erreur lors du parsing JSON")
+    }
+
+    fmt.Println("Token récupéré :", session.Value)  // <== Debug
+
+    c := &client{
+        Token: session.Value,
+    }
+    return c, nil
 }
